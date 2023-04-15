b31abcefca6cf6a3a60aed6a14c09d8b21977407
AlamalWaAlhaya <130256286+AlamalWaAlhaya@users.noreply.github.com>
2023-04-15 03:37:09 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -2,35 +2,61 @@ function list() {
   document.getElementById("arrow").onclick = () => {
     document.getElementById("list").classList.toggle("list-active");
     document.getElementById("arrow").classList.toggle("arrow-anim");
-
   };
 }
 
 list();
 
-let numbers = [
-  '13', '43', '62', '71', '102', '137', '150', '162', '236', '316', '323', '330', '336', '359', '390', '397',
-  '427', '854', '860', '952', '1235', '1289', '1410', '1515', '1549', '1556', '2311', '2319', '2451',
-  '2537', '358', '320', '2376', '185', '312', '90', '1639'
-];
 let search = document.getElementById("search");
 
+let numbers = [];
+let new_numbers = [];
 
-document.getElementById("find").addEventListener("click", () => {
-  
-  const inputValue = search.value;
-  const englishValue = inputValue.replace(/[٠١٢٣٤٥٦٧٨٩]/g, function(match) {
-    return String.fromCharCode(match.charCodeAt(0) - 1632 + 48);
-  });
-
-  if (numbers.includes(englishValue) && englishValue != "") {
-    document.getElementById(
-      "message"
-    ).innerHTML = `<h1 class = "green">يرجي التوجه الي جمعية الأمل و الحياة بالزرقا لاستلام أمانتك</h1>`;
-  } else if (search.value != "") {
-    document.getElementById(
-      "message"
-    ).innerHTML = `<h1 class = "red">سوف يتم اعلامكم في وقت قريب عند توافر أمانتكم</h1>`;
-  }
-});
-
+fetch("https://raw.githubusercontent.com/scriptvip/Ramadan/main/c.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    numbers = data.split("\n");
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i].includes(",")) {
+        let j = numbers[i].replace(",", "");
+        new_numbers.push(j);
+      } else {
+        let j = numbers[i];
+        new_numbers.push(j);
+      }
+      
+    }
+console.log(new_numbers)
+    document.getElementById("find").addEventListener("click", () => {
+      const inputValue = search.value;
+      const englishValue = inputValue.replace(
+        /[٠١٢٣٤٥٦٧٨٩]/g,
+        function (match) {
+          return String.fromCharCode(match.charCodeAt(0) - 1632 + 48);
+        }
+      );
+      if (document.getElementById("place").value == "volvo") {
+        if (
+          (new_numbers.includes(`${search.value}`) && search.value != "") ||
+          (new_numbers.includes(`"${search.value}"`) && search.value != "")
+        ) {
+          document.getElementById(
+            "message"
+          ).innerHTML = `<h1 class = "green">تنورنا في جمعية الأمل والحياة لاستلام هديتكم.. كل عام وأنتم بخير</h1>`;
+          console.log(`${search.value}#`);
+        } else if (
+          (new_numbers.includes(`${search.value}#`) && search.value != "") ||
+          (new_numbers.includes(`"${search.value}"#`) && search.value != "")
+        ) {
+          document.getElementById(
+            "message"
+          ).innerHTML = `<h1 class = "green">تم الاستلام</h1>`;
+        } else if (search.value != "") {
+          document.getElementById(
+            "message"
+          ).innerHTML = `<h1 class = "red"> كل عام وأنتم بخير ..لا يوجد هدايا حاليا لسيادتكم</h1>`;
+        }
+      }
+    });
+  })
+  .catch((error) => console.error(error));
