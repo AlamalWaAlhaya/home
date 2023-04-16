f9ee8a72a64626809aed55b361ace8ff88aff78c
AlamalWaAlhaya <130256286+AlamalWaAlhaya@users.noreply.github.com>
2023-04-16 02:25:48 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -11,22 +11,27 @@ let search = document.getElementById("search");
 
 let numbers = [];
 let new_numbers = [];
-
-fetch("https://raw.githubusercontent.com/scriptvip/Ramadan/main/c.txt")
+let National_numbers = [];
+let chk_list=[];
+fetch("https://raw.githubusercontent.com/scriptvip/Ramadan/main/test.txt")
   .then((response) => response.text())
   .then((data) => {
     numbers = data.split("\n");
     for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i].includes(",")) {
-        let j = numbers[i].replace(",", "");
+      let S = numbers[i].split(" ")
+      if (S[1]!="0")National_numbers.push(S[1])
+      if (S[2]!="0")National_numbers.push(S[2])
+      if (S[0].includes(",")) {
+        let j = S[0].replace(",", "");
         new_numbers.push(j);
       } else {
-        let j = numbers[i];
+        let j = S[0];
         new_numbers.push(j);
       }
-      
     }
-console.log(new_numbers)
+
+    console.log(new_numbers)
+
     document.getElementById("find").addEventListener("click", () => {
       const inputValue = search.value;
       const englishValue = inputValue.replace(
@@ -35,18 +40,21 @@ console.log(new_numbers)
           return String.fromCharCode(match.charCodeAt(0) - 1632 + 48);
         }
       );
+      
+      if (document.getElementById("way").value == "code") chk_list=new_numbers;
+      else if(document.getElementById("way").value == "num") chk_list=National_numbers;
       if (document.getElementById("place").value == "volvo") {
         if (
-          (new_numbers.includes(`${englishValue}`) && englishValue != "") ||
-          (new_numbers.includes(`"${englishValue}"`) && englishValue != "")
+          (chk_list.includes(`${englishValue}`) && englishValue != "") ||
+          (chk_list.includes(`"${englishValue}"`) && englishValue != "")
         ) {
           document.getElementById(
             "message"
           ).innerHTML = `<h1 class = "green">تنورنا في جمعية الأمل والحياة لاستلام هديتكم.. كل عام وأنتم بخير</h1>`;
-          console.log(`${englishValue}#`);
+          console.log(`${englishValue}#\r`);
         } else if (
-          (new_numbers.includes(`${englishValue}#`) && englishValue != "") ||
-          (new_numbers.includes(`"${englishValue}"#`) && englishValue != "")
+          (chk_list.includes(`${englishValue}#\r`) && englishValue != "") ||
+          (chk_list.includes(`"${englishValue}"#\r`) && englishValue != "")
         ) {
           document.getElementById(
             "message"
@@ -54,7 +62,7 @@ console.log(new_numbers)
         } else if (englishValue != "") {
           document.getElementById(
             "message"
-          ).innerHTML = `<h1 class = "gray"> كل عام وأنتم بخير ..لا يوجد هدايا حاليا لسيادتكم</h1>`;
+          ).innerHTML = `<h1 class = "red"> كل عام وأنتم بخير ..لا يوجد هدايا حاليا لسيادتكم</h1>`;
         }
       }
     });
